823ab897d619e6dc17e9a8e0d9c9604a8545dd8c
Panin <panin1970@gmail.com>
2022-01-28 15:18:33 +0300

    add share

--- Photo/Viewcontrollers/PhotoViewController.swift
+++ Photo/Viewcontrollers/PhotoViewController.swift
@@ -7,7 +7,8 @@
 import CoreImage
 import UIKit
 
-enum Source {
+// MARK: - type source
+enum TypeSource {
     case camera
     case gallary
 }
@@ -15,43 +16,34 @@ enum Source {
 class PhotoViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     @IBOutlet var photoImage: UIImageView!
     
-    var imagePhoto: UIImagePickerController!
+    var imagePhoto: UIImagePickerController! // инициализация UIImagePickerController для работы с камерой и галлереей
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imagePhoto = UIImagePickerController() // инициализировали контролер
+        imagePhoto.delegate = self // указываем что здесь будем использовать методы делегата_ type:
     }
     
     @IBAction func gallaryButton(_ sender: Any) {
-        imagePhoto = UIImagePickerController() // инициализировали контролер
-        imagePhoto.delegate = self // указываем что здесь будем использовать методы делегата
-        imagePhoto.sourceType = .photoLibrary // указываем что используем
-        imagePhoto.allowsEditing = true // разрешение редактирования встроенными методами
-        imagePhoto.modalPresentationStyle = .pageSheet // вид представления встроенных инструментов
-        
-        present(imagePhoto, animated: true, completion: nil) // запуск контроллера
-        
+        fetchImage(.gallary)
     }
     
     @IBAction func photoButton(_ sender: Any) {
-        imagePhoto = UIImagePickerController()
-        imagePhoto.delegate = self
-        imagePhoto.sourceType = .camera
-        imagePhoto.cameraDevice = .front
-        imagePhoto.allowsEditing = true
-        imagePhoto.modalPresentationStyle = .pageSheet
-        present(imagePhoto, animated: true, completion: nil)
-        
- 
+        fetchImage(.camera)
     }
     
     @IBAction func saveButton(_ sender: Any) {
+        let image = photoImage.image!
+        let shareController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        shareController.completionWithItemsHandler = { _, bool, _, _ in
+            if bool { print("Yes")}
+        }
+        present(shareController, animated: true, completion: nil)
     }
     
-        //MARK: -  действия после окончания работы встроенных вью контроллеров
+    //MARK: -  действия после окончания работы встроенных вью контроллеров
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        // проверка какое фото используем, едактируемое или нет
+        // проверка какое фото используем, едактируемое или оригинальное
         if let image = info[.editedImage] as? UIImage {
             imageFilter(image)
         } else {
@@ -61,16 +53,18 @@ class PhotoViewController: UIViewController, UINavigationControllerDelegate, UII
         // выключение встроенного вью контроллера
         imagePhoto.dismiss(animated: true, completion: nil)
     }
+}
+
+extension PhotoViewController {
     
-        // MARK: - применение фильтра и вывод изображения во вью
-    func imageFilter(_ image: UIImage) {
+    // MARK: - применение фильтра и вывод изображения во вью
+    private func imageFilter(_ image: UIImage) {
         let context = CIContext(options: nil)
         
         if let currentFilter = CIFilter(name: "CIPhotoEffectMono") {
             let beginImage = CIImage(image: image)
             currentFilter.setValue(beginImage, forKey: kCIInputImageKey) // ключ определяет определяет входное изображение
-      //      currentFilter.setValue(0.9, forKey: kCIInputIntensityKey) - ключ определяет интенсивность фильта, для Монохрома не нужен
-            
+            //      currentFilter.setValue(0.9, forKey: kCIInputIntensityKey) - ключ определяет интенсивность фильта, для Монохрома не нужен
             if let output = currentFilter.outputImage {
                 if let cgimg = context.createCGImage(output, from: output.extent) {
                     let processedImage = UIImage(cgImage: cgimg)
@@ -78,18 +72,19 @@ class PhotoViewController: UIViewController, UINavigationControllerDelegate, UII
                 }
             }
         }
-        
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - получение изображения в зависимости от типа
+    private func fetchImage(_ type: TypeSource) {
+        switch type {
+        case .camera:
+            imagePhoto.sourceType = .camera
+            imagePhoto.cameraDevice = .front
+        case .gallary:
+            imagePhoto.sourceType = .photoLibrary // указываем что используем
+        }
+        imagePhoto.allowsEditing = true // разрешение редактирования встроенными методами
+        imagePhoto.modalPresentationStyle = .pageSheet // вид представления встроенных инструментов
+        present(imagePhoto, animated: true, completion: nil) // запуск контроллера
     }
-    */
-
 }
